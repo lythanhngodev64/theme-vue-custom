6cf194f3e372473e81277bdf9732a6b582a908e6
LYTHANHNGO\lytha <lythanhngodev@gmail.com>
2025-07-19 22:49:14 +0700

    ok

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// src/main.js
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/css/main.css'; // Đường dẫn đến file CSS chính của bạn
-
-createApp(App).mount('#app');
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-// vue.config.js
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'vuex', // Ví dụ: nếu bạn có vuex
-    // Thêm các dependency khác vào đây nếu cần transpile
-  ]
-})
